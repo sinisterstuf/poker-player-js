4e0742913e41a95697990530d13b93dbc7ef108b
Gabor Buella <gbuella@gmail.com>
2014-09-20 17:08:34 +0200

    bugfix

--- player.js
+++ player.js
@@ -66,7 +66,7 @@ function lameCards(cards) {
 
 module.exports = {
 
-    VERSION: "Jofogas's Zsozsobot v5";
+    VERSION: "Jofogas's Zsozsobot v5",
 
   bet_request: function(g) {
     l("----", this.VERSION);
