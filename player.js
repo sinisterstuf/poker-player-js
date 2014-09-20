da6af79b649adf4328015f29bffe36e153bbcfbe
Gabor Buella <gbuella@gmail.com>
2014-09-20 16:45:44 +0200

    Tweak tactics

--- player.js
+++ player.js
@@ -1,4 +1,5 @@
 var rank = require('./rank');
+var is_game_random = true;
 
 l = console.log;
 
@@ -69,7 +70,7 @@ function lameCards(cards) {
 
 module.exports = {
 
-    VERSION: "Jofogas's Zsozsobot v4.10",
+    VERSION: "Jofogas's Zsozsobot v4.11",
 
   bet_request: function(g) {
     l("----", this.VERSION);
@@ -78,16 +79,22 @@ module.exports = {
     var call = g.current_buy_in - myself["bet"];
     var raise = Math.round(call + g.current_buy_in * (1 + Math.random()));
 
-    /* temp */
-if ((toNum(myself.hole_cards[1].rank) > 10 && toNum(myself.hole_cards[0].rank) > 10) || Math.random() > 0.8) {
-  l("Go all in");
-  return myself.stack;
-}
-else {
-    l("fold");
-    return 0;
-}
 
+
+    if (is_game_random) {
+      if ((toNum(myself.hole_cards[1].rank) > 10 && toNum(myself.hole_cards[0].rank) > 10) || Math.random() > 0.8) {
+        l("Go all in");
+        return myself.stack;
+      }
+      if (Math.random() > 0.7) {
+        l("call");
+        return call;
+      }
+      else {
+        l("fold");
+        return 0;
+      }
+    }
 
 
 
