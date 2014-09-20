710e92667665748297bdd8b5835106dd0cee244f
Siôn Le Roux <sinisterstuf@gmail.com>
2014-09-20 16:04:05 +0200

    Add basic test for player

--- test/player_test.js
+++ test/player_test.js
@@ -1,3 +1,14 @@
+var assert = require("assert")
 var player = require('../player')
+var game_state_sample = require("./data/game-state-example.json")
 
-
+describe("player", function() {
+    describe("#bet_request", function() {
+        it("should return a number", function() {
+            assert.equal(
+                typeof(player.bet_request(JSON.parse( game_state_sample )).toString()),
+                "number"
+            )
+        })
+    })
+})
