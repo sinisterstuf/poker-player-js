6a012523b154038dad25ae4fad6e02de24bdb8cb
Siôn Le Roux <sinisterstuf@gmail.com>
2014-09-20 12:59:38 +0200

    Add logic to fold/call before flop

--- player.js
+++ player.js
@@ -42,17 +42,45 @@ function compute_chip_factor(g) {
     }
 }
 
+function lameCards(cards) {
+    // return TRUE if the cards are LAME
+    if (
+        toNum(cards[0].rank) < 9 &&
+        toNum(cards[1].rank) < 9 &&
+        cards[0].rank != cards[1].rank
+       ) {
+        return true
+    } else {
+        return false
+    }
+}
+
 module.exports = {
 
-    VERSION: "Jofogas's Zsozsobot v3.3",
+    VERSION: "Jofogas's Zsozsobot v4",
 
   bet_request: function(g) {
     var myself = g.players[g.in_action];
+    var call = g.current_buy_in - myself["bet"];
+    var min_raise = call + g.minimum_raise;
 
     // just in case!!!
     if (myself.status != "active") {
         return "OK";
     }
+
+    // don't bet before flop if our hand is crap
+    if (g.community_cards.length < 1) {
+        // we're before flop
+        if (lameCards(myself.hole_cards)) {
+            // fold
+            return 0;
+        } else
+            // call
+            return call;
+        }
+    }
+
     if (myself.hole_cards[0].rank == myself.hole_cards[1].rank) {
         l("Go all in");
         return myself.stack;
@@ -64,7 +92,7 @@ module.exports = {
 
     if (myself.stack > average_stack * 0.7) {
         /* brave */
-        var bet = g.current_buy_in - myself["bet"] + g.minimum_raise * 2;
+        var bet = min_raise * 2;
         l("fearlessly raising: ", bet);
         return bet;
     }
