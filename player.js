c872597891d0494073809d3269c7e13e0ffc5ab5
Gabor Buella <gbuella@gmail.com>
2014-09-20 14:02:25 +0200

    urgent bugfix

--- player.js
+++ player.js
@@ -69,7 +69,7 @@ module.exports = {
     VERSION: "Jofogas's Zsozsobot v4",
 
   bet_request: function(g) {
-    l("----", VERSION);
+    l("----");
     l(new Date());
     var myself = g.players[g.in_action];
     var call = g.current_buy_in - myself["bet"];
