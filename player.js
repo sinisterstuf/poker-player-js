b2306892580072e6946101f87dad6c056591d4e6
Siôn Le Roux <sinisterstuf@gmail.com>
2014-09-20 10:33:12 +0200

    Re-indent

--- player.js
+++ player.js
@@ -1,14 +1,14 @@
 
 module.exports = {
 
-  VERSION: "Jofogas's Zsozsobot v2",
+    VERSION: "Jofogas's Zsozsobot v2",
 
-  bet_request: function(g) {
-    bet = g.current_buy_in - g.players[g.in_action]["bet"] + g.minimum_raise;
-    return bet;
-  },
+    bet_request: function(g) {
+        bet = g.current_buy_in - g.players[g.in_action]["bet"] + g.minimum_raise;
+        return bet;
+    },
 
-  showdown: function(g) {
+    showdown: function(g) {
 
-  }
+    }
 };
