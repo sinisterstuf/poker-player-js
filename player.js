db0ac1ff6a2dcb1dfd3237a7e5122bad7e44ca48
Siôn Le Roux <sinisterstuf@gmail.com>
2014-09-20 16:47:35 +0200

    minor fixes

--- player.js
+++ player.js
@@ -1,21 +1,17 @@
 var rank = require('./rank');
 
-l = console.log;
+var l = console.log;
 
 function toNum(c) {
-    switch(c) {
+    switch (c) {
         case 'J':
             return 11;
-            break;
         case 'Q':
             return 12;
-            break;
         case 'K':
             return 13;
-            break;
         case 'A':
             return 14;
-            break;
 
         default:
             return c;
